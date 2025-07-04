5b179e71141cee3e150d1edd9b1a561db02d0025
Jalin Hu <1150710274@qq.com>
2025-07-04 09:21:25 +0000

    bert_learning

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from transformers import pipeline
 
 def main():
     print("Hello, world!")
-    classifier = pipeline("sentiment-analysis",model="openai-community/gpt2")
     
 
 if __name__ == "__main__":
