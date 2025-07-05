abe439d93d723e7f48a396d124bfac0529f982fb
Jalin Hu <1150710274@qq.com>
2025-07-05 14:23:01 +0000

    提交

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from transformers import pipeline
-
+import torch
 def main():
     print("Hello, world!")
     
 
 if __name__ == "__main__":
     main()
+def bert():
+
+pipeline = pipeline(
+    task="fill-mask",
+    model="google-bert/bert-base-uncased",
+    torch_dtype=torch.float16,
+    device=0
+)
+result =  pipeline("sun arise from the [MASK]")
+print(result)
