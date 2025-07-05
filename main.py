c59a81ba0bed1eee816f6f123b0232af72642914
Jalin Hu <1150710274@qq.com>
2025-07-05 14:54:36 +0000

    修改

--- main.py
+++ main.py
@@ -6,13 +6,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-def bert():
-
-pipeline = pipeline(
-    task="fill-mask",
-    model="google-bert/bert-base-uncased",
-    torch_dtype=torch.float16,
-    device=0
-)
-result =  pipeline("sun arise from the [MASK]")
-print(result)
